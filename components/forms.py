2c2aeffeeb396eab2623d6c3e0c997c9e11e567b
romaine_noir <davianrgray@gmail.com>
2026-01-29 12:03:41 +0000

    added validators to forms

--- components/forms.py
+++ components/forms.py
@@ -1,10 +1,12 @@
 from wtforms import FileField, SubmitField
 from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired
+'''Flask Forms For Application'''
 
 # create a form for converting files in txt format
 class TXTConvert(FlaskForm):
     '''a form for converting files in txt format'''
-    file_doc = FileField("Upload TXT")
+    file_doc = FileField("Upload TXT", validators=[DataRequired()])
     
     convert_to_pdf = SubmitField("Convert To .pdf File")
     convert_to_csv = SubmitField("Convert To .csv File")
@@ -13,7 +15,7 @@ class TXTConvert(FlaskForm):
 # create a form for converting files in doc format
 class DOCConvert(FlaskForm):
     '''a form for converting files in doc format'''
-    file_doc = FileField("Upload DOC")
+    file_doc = FileField("Upload DOC", validators=[DataRequired()])
     
     convert_to_pdf = SubmitField("Convert To .pdf File")
     convert_to_txt = SubmitField("Convert To .txt File")
@@ -22,7 +24,7 @@ class DOCConvert(FlaskForm):
 # create a form for converting files in pdf format
 class PDFConvert(FlaskForm):
     '''a form for converting files in pdf format'''
-    file_doc = FileField("Upload PDF")
+    file_doc = FileField("Upload PDF", validators=[DataRequired()])
     
     convert_to_txt = SubmitField("Convert To .txt File")
     convert_to_csv = SubmitField("Convert To .csv File")
@@ -31,7 +33,7 @@ class PDFConvert(FlaskForm):
 # create a form for converting files in csv format
 class CSVConvert(FlaskForm):
     '''a form for converting files in csv format'''
-    file_doc = FileField("Upload PDF")
+    file_doc = FileField("Upload CSV", validators=[DataRequired()])
     
     convert_to_txt = SubmitField("Convert To .txt File")
     convert_to_doc = SubmitField("Convert To .doc File")
