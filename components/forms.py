f0b97a5baed1ee3430f825434ac01e25e1c041a4
romaine_noir <davianrgray@gmail.com>
2026-01-29 12:21:04 +0000

    edited validators after suggestion

--- components/forms.py
+++ components/forms.py
@@ -1,12 +1,15 @@
 from wtforms import FileField, SubmitField
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm,
 from wtforms.validators import DataRequired
+from flask_wtf.file import FileRequired, FileAllowed
 '''Flask Forms For Application'''
 
 # create a form for converting files in txt format
 class TXTConvert(FlaskForm):
     '''a form for converting files in txt format'''
-    file_doc = FileField("Upload TXT", validators=[DataRequired()])
+    file_doc = FileField("Upload TXT", validators=[
+        FileRequired(),
+        FileAllowed(['txt'], "Unsupported File")])
     
     convert_to_pdf = SubmitField("Convert To .pdf File")
     convert_to_csv = SubmitField("Convert To .csv File")
@@ -15,7 +18,9 @@ class TXTConvert(FlaskForm):
 # create a form for converting files in doc format
 class DOCConvert(FlaskForm):
     '''a form for converting files in doc format'''
-    file_doc = FileField("Upload DOC", validators=[DataRequired()])
+    file_doc = FileField("Upload DOC", validators=[
+        FileRequired(),
+        FileAllowed(['doc', 'docx'], "Unsupported File")])
     
     convert_to_pdf = SubmitField("Convert To .pdf File")
     convert_to_txt = SubmitField("Convert To .txt File")
@@ -24,7 +29,9 @@ class DOCConvert(FlaskForm):
 # create a form for converting files in pdf format
 class PDFConvert(FlaskForm):
     '''a form for converting files in pdf format'''
-    file_doc = FileField("Upload PDF", validators=[DataRequired()])
+    file_doc = FileField("Upload PDF", validators=[
+        FileRequired(),
+        FileAllowed(['pdf'], "Unsupported File")])
     
     convert_to_txt = SubmitField("Convert To .txt File")
     convert_to_csv = SubmitField("Convert To .csv File")
@@ -33,7 +40,9 @@ class PDFConvert(FlaskForm):
 # create a form for converting files in csv format
 class CSVConvert(FlaskForm):
     '''a form for converting files in csv format'''
-    file_doc = FileField("Upload CSV", validators=[DataRequired()])
+    file_doc = FileField("Upload CSV", validators=[
+        FileRequired(),
+        FileAllowed(['csv'], "Unsupported File")])
     
     convert_to_txt = SubmitField("Convert To .txt File")
     convert_to_doc = SubmitField("Convert To .doc File")
