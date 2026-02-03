c1ef577521cdb5ad5f7617f014699f75365ee868
romaine_noir <davianrgray@gmail.com>
2026-02-03 21:24:52 +0000

    refactored pdf class

--- components/functions.py
+++ components/functions.py
@@ -2,6 +2,26 @@ from PyPDF2 import PdfReader
 import textcase
 
 # create function to extract text from PDF
+
+class PDF_EXTRACT():
+    
+    # PDF TO TXT
+    def PDF_to_TXT(file_=str):
+        """ PDF TO TXT
+            (file_ = name of the file)"""
+        file_reader = PdfReader(file_)
+        page_num = file_reader._get_num_pages()
+        extracted_text = ""
+        file_name_title = textcase.snake(file_reader.metadata.title) + '.txt'
+        file_name_title.strip()
+        for num in range(page_num):
+            text_ = file_reader.pages[num]
+            extracted_text += text_.extract_text()
+        
+        with open(file_name_title, 'w') as file:
+            file.write(extracted_text)
+        
+
 def PDF_to_TXT(file_=str):
     """_summary_
         CONVERTS PDFs TO TXTs
@@ -22,4 +42,5 @@ def PDF_to_TXT(file_=str):
         
 
     
-PDF_to_TXT("pdf_dummi.pdf")
+new_file = PDF_EXTRACT
+new_file.PDF_to_TXT("pdf_dummi.pdf")
