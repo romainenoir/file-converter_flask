e256b837f86ddecddd13c9806998818a1c951701
romaine_noir <davianrgray@gmail.com>
2026-02-25 08:05:57 +0000

    changed form variable names

--- app.py
+++ app.py
@@ -15,41 +15,41 @@ def homepage():
 def convert(file_type):
     file_type = file_type.upper()
     user_upload = None
-    doc_converter = DOCConvert()
-    pdf_converter = PDFConvert()
-    txt_converter = TXTConvert()
-    csv_converter = CSVConvert()
+    doc_converter_form = DOCConvert()
+    pdf_converter_form = PDFConvert()
+    txt_converter_form = TXTConvert()
+    csv_converter_form = CSVConvert()
     
-    if pdf_converter.validate_on_submit():
+    if pdf_converter_form.validate_on_submit():
         convert_pdf_ = PDF_CONVERT()
-        user_upload = pdf_converter.file_doc.data
+        user_upload = pdf_converter_form.file_doc.data
         
         # CONVERT PDF TO TXT & SEND BACK TO USER
-        if pdf_converter.convert_to_txt.data:
+        if pdf_converter_form.convert_to_txt.data:
             file_stream, file_name_title = convert_pdf_.PDF_to_TXT(user_upload)
             return send_file(path_or_file=file_stream, as_attachment=True, download_name=file_name_title)
         
-        elif pdf_converter.convert_to_doc.data:
+        elif pdf_converter_form.convert_to_doc.data:
             file_stream, file_name_title = convert_pdf_.PDF_to_DOC(user_upload)
             return send_file(path_or_file=file_stream, as_attachment=True, download_name=file_name_title)
         
-        elif pdf_converter.convert_to_csv.data:
+        elif pdf_converter_form.convert_to_csv.data:
             file_stream, file_name_title = convert_pdf_.PDF_to_CSV(user_upload)
             return send_file(path_or_file=file_stream, as_attachment=True, download_name=file_name_title)
         
-    elif txt_converter.validate_on_submit():
-        user_upload = txt_converter.file_doc.data
+    elif txt_converter_form.validate_on_submit():
+        user_upload = txt_converter_form.file_doc.data
         pass
-    elif csv_converter.validate_on_submit():
-        user_upload = csv_converter.file_doc.data
+    elif csv_converter_form.validate_on_submit():
+        user_upload = csv_converter_form.file_doc.data
         pass
     
     return render_template("file.html", file_types=file_types, 
                            file_type=file_type, 
-                           doc_converter=doc_converter,
-                           pdf_converter=pdf_converter, 
-                           txt_converter=txt_converter, 
-                           csv_converter=csv_converter,)
+                           doc_converter_form=doc_converter_form,
+                           pdf_converter_form=pdf_converter_form, 
+                           txt_converter_form=txt_converter_form, 
+                           csv_converter_form=csv_converter_form,)
 
 @app.route('/about')
 def about():
